e202b0d1effda04f73afed1ee10d29c42ab2d549
cleyson <cleysoncassio@gmail.com>
2021-08-25 20:19:29 -0300

    atualização travis.yml

--- appweb/base/tests/test_home.py
+++ appweb/base/tests/test_home.py
@@ -2,5 +2,5 @@ from django.test import Client
 
 
 def test_status_code(client: Client):
- resp = client.get('/')
- assert resp.status_code == 200
+    resp = client.get('/')
+    assert resp.status_code == 200
