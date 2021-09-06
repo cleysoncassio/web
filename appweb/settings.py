b39cde24c03a7f00906b331e4247ea5aed298c19
cleyson <cleysoncassio@gmail.com>
2021-09-06 16:09:16 -0300

    test

--- appweb/settings.py
+++ appweb/settings.py
@@ -150,19 +150,19 @@ if AWS_ACCESS_KEY_ID:
 # STATIC ASSETS
 # ----------------------------------------------------------------------
 
-STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
-STATIC_S3_PATH = '/static/'
-STATIC_ROOT = f'/{STATIC_S3_PATH}/'
-STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+    STATICFILES_STORAGE = 's3_folder_storage.s3.StaticStorage'
+    STATIC_S3_PATH = '/static/'
+    STATIC_ROOT = f'/{STATIC_S3_PATH}/'
+    STATIC_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{STATIC_S3_PATH}/'
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 # UPLOAD MEDIA FOLDER
 # ----------------------------------------------------------------------
 
-DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
-DEFAULT_S3_PATH = '/media/'
-MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
-MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}/'
+    DEFAULT_FILE_STORAGE = 's3_folder_storage.s3.DefaultStorage'
+    DEFAULT_S3_PATH = '/media/'
+    MEDIA_ROOT = f'/{DEFAULT_S3_PATH}/'
+    MEDIA_URL = f'//s3.amazonaws.com/{AWS_STORAGE_BUCKET_NAME}/{DEFAULT_S3_PATH}/'
 
 INSTALLED_APPS.append('s3_folder_storage')
 INSTALLED_APPS.append('storages')
